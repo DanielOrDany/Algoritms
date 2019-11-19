1f53957a652dbd4e03da1646c0e116dde8688be7
Daniel <daniel.nikylshyn@gmail.com>
2019-11-20 00:52:46 +0200

    Upgrade: lab4 optimizated

--- lab4/govern.py
+++ lab4/govern.py
@@ -6,83 +6,104 @@ from collections import defaultdict
 
 class Graph:
     def __init__(self):
-        self.edges = defaultdict(list)
-        self.docs = []
-        self.root = 0
+        self.graph = defaultdict(list)
 
-    def add_edge(self, v, e):
-        self.edges[v].append(e)
-        self.edges[e] = self.edges[e]
-        if v not in self.docs:
-            self.docs.append(v)
-        if e not in self.docs:
-            self.docs.append(e)
-
-    def visit_document(self, v, visited, sort_list):
-        visited[v] = True
-        for neighbour in self.edges[v]:
-            if not visited[neighbour]:
-                self.visit_document(neighbour, visited, sort_list)
-
-        sort_list.append(v)
-
-    def find_order(self):
-        if self.is_cyclic():
-            return "graph has a cycle"
-
-        received_documents = {document: False for document in self.edges}
-        sort_list = []
-
-        for v in self.edges:
-            if not received_documents[v]:
-                self.visit_document(v, received_documents, sort_list)
-
-        return sort_list
+    def add_edge(self, u, v):
+        self.graph[u].append(v)
 
     def is_cyclic(self):
-        visited = [False] * len(self.docs)
-        rec_stack = [False] * len(self.docs)
+        visited = [False] * (len(self.graph) + 1)
+        rec_stack = [False] * (len(self.graph) + 1)
 
-        for node in self.docs:
-            if not visited[self.docs.index(node)]:
+        for node in range((len(self.graph) + 1)):
+            if not visited[node]:
                 if self.is_cyclic_util(node, visited, rec_stack):
                     return True
 
         return False
 
     def is_cyclic_util(self, v, visited, rec_stack):
-        visited[self.docs.index(v)] = True
-        rec_stack[self.docs.index(v)] = True
+        visited[v] = True
+        rec_stack[v] = True
 
-        for neighbour in self.edges[v]:
-            if not visited[self.docs.index(neighbour)]:
+        for neighbour in self.graph[v]:
+            if not visited[neighbour]:
                 if self.is_cyclic_util(neighbour, visited, rec_stack):
                     return True
 
         return False
 
+    def dfs(self, v, visited, stack):
+
+        if self.is_cyclic():
+            return "graph has a cycle"
+
+        visited[v] = True
+
+        for i in self.graph[v]:
+            if visited[i] == False:
+                self.dfs(i, visited, stack)
+
+        stack.insert(0, v)
+
+
+    def sort_documents(self):
+
+        visited = [False] * (len(self.graph) + 1)
+        stack = []
+
+        for i in range((len(self.graph) + 1)):
+            if visited[i] == False:
+                self.dfs(i, visited, stack)
+
+        return stack
+
 
 class TestStringMethods(unittest.TestCase):
-
     def test_file_ex(self):
         self.assertEqual(os.path.exists('./govern.out'), True)
 
     def test_file_ex(self):
         self.assertEqual(filecmp.cmp('govern.out', 'right.out'), True)
 
+
 if __name__ == '__main__':
     g = Graph()
+
+    documents = {}
+
     data = open("govern.in", "r")
     contents = data.readlines()
 
+    number = 0
     for document in contents:
         res = [i for i in document.split()]
-        g.add_edge(res[0], res[1])
+        if res[0] not in documents.keys():
+            documents.update({res[0]: number})
+            number = number + 1
+
+    for document in contents:
+        res = [i for i in document.split()]
+        if res[1] not in documents.keys():
+            documents.update({res[1]: number})
+            number = number + 1
+
+    print "\n\nConvert documents to numbers: \n"
+
+    for document in contents:
+        res = [i for i in document.split()]
+        print str(documents[res[0]]) + ": " + str(res[0]) + " | " + str(documents[res[1]]) + ": " + str(res[1])
+        g.add_edge(documents[res[1]], documents[res[0]])
+
 
     f = open("govern.out", "w+")
-    for item in g.find_order():
-        f.write(item + "\n")
+
+    for g_ed in g.sort_documents():
+        for v, ed in documents.items():
+            if ed == g_ed:
+                f.write(v + "\n")
+
     f.close()
+    data.close()
 
     unittest.main()
-
