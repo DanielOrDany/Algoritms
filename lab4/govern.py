afcc5d39fe77becf2335ec48054cc1db07d85f0f
Daniel <daniel.nikylshyn@gmail.com>
2019-11-20 12:49:45 +0200

    Upgrade: lab4 update find_circle method (add rec_stack)

--- lab4/govern.py
+++ lab4/govern.py
@@ -11,48 +11,47 @@ class Graph:
     def add_edge(self, u, v):
         self.graph[u].append(v)
 
-    def is_cyclic(self):
-        visited = [False] * (len(self.graph) + 1)
-        rec_stack = [False] * (len(self.graph) + 1)
-
-        for node in range((len(self.graph) + 1)):
-            if not visited[node]:
-                if self.is_cyclic_util(node, visited, rec_stack):
-                    return True
-
-        return False
-
     def is_cyclic_util(self, v, visited, rec_stack):
         visited[v] = True
         rec_stack[v] = True
 
         for neighbour in self.graph[v]:
-            if not visited[neighbour]:
-                if self.is_cyclic_util(neighbour, visited, rec_stack):
+            if visited[neighbour] == False:
+                if self.is_cyclic_util(neighbour, visited, rec_stack) == True:
                     return True
+            elif rec_stack[neighbour] == True:
+                return True
 
+        rec_stack[v] = False
+        return False
+
+    def is_cyclic(self):
+        visited = [False] * (len(self.graph) + 1)
+        rec_stack = [False] * (len(self.graph) + 1)
+
+        for node in range(len(self.graph) + 1):
+            if visited[node] == False:
+                if self.is_cyclic_util(node, visited, rec_stack) == True:
+                    return True
         return False
 
     def dfs(self, v, visited, stack):
-
-        if self.is_cyclic():
-            return "graph has a cycle"
-
         visited[v] = True
 
-        for i in self.graph[v]:
+        for i in self.graph[v]: #search in neighbours
             if visited[i] == False:
                 self.dfs(i, visited, stack)
 
         stack.insert(0, v)
 
-
     def sort_documents(self):
+        if self.is_cyclic():
+            return "graph has a cycle"
 
-        visited = [False] * (len(self.graph) + 1)
+        visited = [False] * (len(self.graph) + 1)   #7/8, +1
         stack = []
 
-        for i in range((len(self.graph) + 1)):
+        for i in range(len(self.graph) + 1):
             if visited[i] == False:
                 self.dfs(i, visited, stack)
 
@@ -60,10 +59,10 @@ class Graph:
 
 
 class TestStringMethods(unittest.TestCase):
-    def test_file_ex(self):
+    def test_file_exist(self):
         self.assertEqual(os.path.exists('./govern.out'), True)
 
-    def test_file_ex(self):
+    def test_right_answer(self):
         self.assertEqual(filecmp.cmp('govern.out', 'right.out'), True)
 
 
@@ -88,20 +87,17 @@ if __name__ == '__main__':
             documents.update({res[1]: number})
             number = number + 1
 
-    print "\n\nConvert documents to numbers: \n"
-
     for document in contents:
         res = [i for i in document.split()]
         print str(documents[res[0]]) + ": " + str(res[0]) + " | " + str(documents[res[1]]) + ": " + str(res[1])
         g.add_edge(documents[res[1]], documents[res[0]])
-
 
     f = open("govern.out", "w+")
 
     for g_ed in g.sort_documents():
         for v, ed in documents.items():
             if ed == g_ed:
-                f.write(v + "\n")
+                f.write(v + "\n") #\n after last item, done!
 
     f.close()
     data.close()
